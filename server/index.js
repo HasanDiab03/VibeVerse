dba2e475c6a8aec1a7bdfca201395d00d25f8a0c
Hasan Diab <hasandiab.main@gmail.com>
2023-08-30 19:33:34 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -9,11 +9,7 @@ const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://vibeverse.netlify.app"],
-  })
-);
+app.use(cors());
 
 app.use("/posts", postRoutes);
 
